bbc79e57815b5777a99f5a8c4bf9bc9a19f101bb
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-05-30 19:21:50 +0200

    Sets up initial styled components

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,18 +1,6 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from "styled-components";
 
-/* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
-
+const GlobalStyles = createGlobalStyle`
 :root {
   /* Indigo */
   --color-brand-50: #eef2ff;
@@ -153,9 +141,8 @@ img {
 
   /* For dark mode */
   filter: grayscale(var(--image-grayscale)) opacity(var(--image-opacity));
-}
 
-/*
+  /*
 FOR DARK MODE
 
 --color-grey-0: #18212f;
@@ -194,3 +181,8 @@ FOR DARK MODE
 --image-grayscale: 10%;
 --image-opacity: 90%;
 */
+
+}
+`;
+
+export default GlobalStyles;
