0cbc58a45b13b4d4cd7e595a9e2348b7485fada0
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-06-05 19:41:20 +0200

    implements modal window for CreateCabinForm

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import CreateCabinForm from "./CreateCabinForm";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+
+function AddCabin() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setIsOpenModal((open) => !open)}>
+        Add New Cabin
+      </Button>
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default AddCabin;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,9 @@
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useState } from "react";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -16,8 +13,7 @@ function Cabins() {
 
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm(!showForm)}>Add New Cabin</Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
